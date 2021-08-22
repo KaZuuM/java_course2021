34f1423889134bc65292446954bd7f179790a509
KaZuuM <89159227+KaZuuM@users.noreply.github.com>
2021-08-22 17:16:50 +0300

    create area function

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,17 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        hello("world");
+        double len = 5;
+        System.out.println( "Площадь квадрата со стороной " + len + " = " + area(len) );
     }
 
+    public static void hello(String somebody) {
+
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double area(double l) {
+        return l * l;
+    }
 }
